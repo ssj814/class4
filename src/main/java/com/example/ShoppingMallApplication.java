0a629d524f73e5bd1596a82a7aba716fb692e243
S.SeokJin <40196685+ssj814@users.noreply.github.com>
2024-10-20 00:49:34 +0900

    Revert "Member->main"

--- src/main/java/com/example/ShoppingMallApplication.java
+++ src/main/java/com/example/ShoppingMallApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-// 자동으로 제공되는 로그인페이지 사용안함
+@SpringBootApplication
 public class ShoppingMallApplication {
 
 	public static void main(String[] args) {

71aba82728d5a288835ce04375df47441890d0ed
hwayoung <hwyoung123@naver.com>
2024-12-05 23:26:19 +0900

    imageService에 여러개 이미지 처리함수 생성

--- src/main/java/com/example/service/image/ImageService.java
+++ src/main/java/com/example/service/image/ImageService.java
@@ -2,21 +2,14 @@ package com.example.service.image;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.StringJoiner;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.dto.ProductDTO;
-import com.example.dto.ProductOptionDTO;
-import com.example.service.shoppingmall.ProductService;
-
-import lombok.RequiredArgsConstructor;
+import com.example.dto.ProductReviewDTO;
 
 @Service
 public class ImageService {
@@ -25,7 +18,6 @@ public class ImageService {
 
 	// 이미지 저장
 	public String saveImg(MultipartFile image, String subDir) {
-
 		String uploadDir = baseDir + subDir + File.separator;
 		UUID uuid = UUID.randomUUID();
 		String imgName = uuid + image.getOriginalFilename();
@@ -48,6 +40,27 @@ public class ImageService {
 		return imgName;
 	}
 	
+	// 이미지 여러개 저장
+	public String saveMultipleImg(MultipartFile[] images, String subDir) {
+	    StringJoiner imageNames = new StringJoiner(","); // 각 이미지 이름 사이에 콤마를 구분자로 사용
+
+		try {
+			if (!images[0].isEmpty()) {
+				for (MultipartFile img : images) {
+					String imgName = saveImg(img, subDir);
+					//저장된 이미지 이름을 StringJoiner에 추가
+					if (imgName != null) {
+						imageNames.add(imgName);
+	                }
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return imageNames.toString();
+	}
+	
 	// 이미지 삭제
 	public void deleteImg(String imgName, String subDir) {
 		String uploadDir = baseDir + subDir + File.separator;
@@ -55,14 +68,37 @@ public class ImageService {
 		file.delete(); // 파일이미지 삭제
 	}
 	
+	// 이미지 여러개 삭제
+	public void deleteMultipleImg(String imgNames, String subDir) {
+		if (imgNames != null) {
+			String[] imgNamesArray  = imgNames.split(",");
+			for (String imgName : imgNamesArray ) {
+				deleteImg(imgName,subDir);
+			}
+		}
+	}
+	
 	// 이미지 수정
 	@Transactional
 	public String updateImg(MultipartFile image, String preImgName, String subDir) {
-		String imgName = null;
+		String imgName = preImgName;
 		
 		if(!image.isEmpty()) {
 			deleteImg(preImgName,subDir);
 			imgName = saveImg(image,subDir);
+		}
+		
+		return imgName;
+	}
+	
+	// 이미지 여러개 수정
+	@Transactional
+	public String updateMultipleImg(MultipartFile[] images, String preImgNames, String subDir) {
+		String imgName = preImgNames;
+		
+		if (!images[0].isEmpty()) {
+			deleteMultipleImg(preImgNames, subDir);
+			imgName = saveMultipleImg(images,subDir);
 		}
 		
 		return imgName;
